2cd969fc8901f4db7e51ffbccb492fb4a1dab52f
nbkhope <ryumemaru@yahoo.com>
2024-05-06 21:22:24 -0400

    Implement audio playlist

--- audio-playlist/script.js
+++ audio-playlist/script.js
@@ -1,1 +1,13 @@
 
+function playItem(event) {
+	
+  document.getElementById('audio-player').src = event.target.dataset.url;
+  document.getElementById('audio-player').play();
+  
+  const collection = document.getElementsByClassName('active')
+  for(const playlistItem of collection) {
+  	playlistItem.classList.remove('active');
+  }
+  event.target.classList.add('active');
+  document.getElementById('current-song').textContent = event.target.textContent
+}
